f3e44185b896228421d6a6d0be46b5e050e7de13
demitech1024 <demitech1024@gmail.com>
2020-01-31 14:43:13 -0500

    completed 9 maybe

--- src/Exercises.java
+++ src/Exercises.java
@@ -190,7 +190,26 @@ public class Exercises {
 	}
 	
 	public boolean balance(ArrayList<Integer> numbers) {
-		// write your code here
+		if (numbers == null || numbers.size() < 2) {
+			return false;
+		}
+
+		for (int i = 0; i < numbers.size() - 1; i++) {
+			int jSum = 0;
+			int kSum = 0;
+
+			for (int j = i; j >= 0; j--) {
+				jSum += numbers.get(j);
+			}
+
+			for (int k = i + 1;  k < numbers.size(); k++) {
+				kSum += numbers.get(k);
+			}
+
+			if (jSum == kSum) {
+				return true;
+			}
+		}
 		
 		return false;	// default return value to ensure compilation
 	}
